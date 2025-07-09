e6da610f3d66798eba6ca17c35cbc56a0deb9a6c
Simon Ndege <simopetley@gmail.com>
2025-07-09 17:05:23 +0300

    add changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,14 @@
 import React from "react";
-// EITHER this:
-// import myImage from "../images/demo.png";
-
-// OR this (using public folder):
-// (delete the import and use src="/images/demo.png")
+import demoImage from "../images/demo.png"; // adjust the path relative to About.js
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-     
-      <img src="/images/demo.png" alt="I made this" />
-    
+      <img src={demoImage} alt="I made this" />
     </div>
   );
 }
+
 export default About;
